64e34e05a3470b6c68e87e5e884600beafe5ba6b
Muhammad Rizqy Amrullah <rizkii.amr@gmail.com>
2025-04-01 17:39:24 +0700

    feat: add todos

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	// todo: parse aws creds from aws-default environment variables
+	// - AWS_ACCESS_KEY_ID
+	// - AWS_SECRET_ACCESS_KEY
+	// - AWS_SESSION_TOKEN (optional)
+	// - AWS_DEFAULT_REGION (optional, default: us-east-1)
+
+	// good to have: detect Web Identity Token / IRSA 
+
+	// todo: call `aws sts get-caller-identity` equivalent method, and parse the result
 	fmt.Println("hello world")
 }
