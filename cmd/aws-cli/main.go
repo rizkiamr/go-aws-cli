2609c995cd127311a132f9200c5bd64ad8be802a
Muhammad Rizqy Amrullah <rizkii.amr@gmail.com>
2025-04-02 07:40:58 +0700

    feat: add debugMode

--- cmd/aws-cli/main.go
+++ cmd/aws-cli/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/rizkiamr/go-aws-cli/internal/model"
-
 	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/aws/smithy-go/logging"
+	"github.com/rizkiamr/go-aws-cli/internal/model"
 )
 
 func main() {
@@ -18,9 +21,18 @@ func main() {
 	// using the SDK's default configuration, load additional config
 	// and credentials values from the environment variables, shared
 	// credentials, and shared configuration files
+
+	debugMode := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithDefaultRegion("us-east-1"))
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	if *debugMode {
+		cfg.Logger = logging.NewStandardLogger(os.Stdout) // Sends logs to stdout
+		cfg.ClientLogMode = aws.LogRequestWithBody | aws.LogResponseWithBody
 	}
 
 	svc := sts.NewFromConfig(cfg)
